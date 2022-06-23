af2ae846f14178570bd3e32be531cedb403148b3
shawn-stover <shawn.stover.it.cs@gmail.com>
2022-06-23 02:21:19 -0700

    ashley-website

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Creatively Cozy Creations</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
