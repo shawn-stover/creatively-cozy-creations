c23bfb5591f414b5de5630f92246d41aee4990ab
shawn-stover <shawn.stover.it.cs@gmail.com>
2022-06-23 05:44:52 -0700

    Removed data.js from frontend
    Removed data.js from frontend and updated Homescreen.js using useReducer to communicate for slower internet speeds about loading or errors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Served at http://localhost:${port}`);
 });
