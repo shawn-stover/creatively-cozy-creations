41be176c0411012cd8bb69f9b9e189a2ef41b93d
shawn-stover <shawn.stover.it.cs@gmail.com>
2022-07-13 01:36:31 -0700

    Added user Route
    user Route for authentication and utils for ExpressAsyncHandler

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 // Imports
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -18,8 +18,19 @@ mongoose
   });
 
 const app = express();
+
+// POST request is converted to JSON object
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+// Middleware for ExpressASyncHandler
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
