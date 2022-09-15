85f7984cced9cb17acacff98456af582e77ad177
shawn-stover <shawn.stover.it.cs@gmail.com>
2022-09-14 18:16:00 -0700

    Implement Sidebar and Search Box
    Implemented Sidebar and Search Box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // Imports
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
@@ -10,7 +10,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from './Store.js';
 import CartScreen from './screens/CartScreen.js';
 import SignInScreen from './screens/SignInScreen.js';
@@ -21,6 +21,10 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen.js';
 import OrderScreen from './screens/OrderScreen.js';
 import OrderHistoryScreen from './screens/OrderHistoryScreen.js';
 import ProfileScreen from './screens/ProfileScreen.js';
+import Button from 'react-bootstrap/Button';
+import { getError } from './utils.js';
+import axios from 'axios';
+import SearchBox from './components/SearchBox.js';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -34,13 +38,40 @@ function App() {
     window.location.href = '/signin';
   };
 
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div
+        className={
+          sidebarIsOpen
+            ? 'd-flex flex-column site-container active-cont'
+            : 'd-flex flex-column site-container'
+        }
+      >
         <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar className="color-nav" expand="lg">
             <Container>
+              <Button
+                variant="outline-dark"
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className="fas fa-bars"></i>
+              </Button>
               <LinkContainer to="/">
                 <Navbar.Brand className="color-brand">
                   Creatively Cozy Creations
@@ -48,6 +79,7 @@ function App() {
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBox />
                 <Nav className="me-auto w-100 justify-content-end">
                   <Link to="/cart" className="mt-2 nav-Link">
                     Cart
@@ -91,6 +123,29 @@ function App() {
             </Container>
           </Navbar>
         </header>
+        <div
+          className={
+            sidebarIsOpen
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-black w-100 p-2">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={`/search?category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
         <main>
           <Container className="mt-3">
             <Routes>
---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,35 @@
+// Imports
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { useNavigate } from 'react-router-dom';
+
+export default function SearchBox() {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate(query ? `/search/?querry=${query}` : '/search');
+  };
+
+  return (
+    <Form className="d-flex me-auto" onSubmit={submitHandler}>
+      <InputGroup>
+        <FormControl
+          type="text"
+          name="q"
+          id="q"
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search Products"
+          aria-label="Search Products"
+          aria-describedby="button-search"
+        ></FormControl>
+        <Button variant="outline-dark" type="submit" id="button-search">
+          <i className="fas fa-search"></i>
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+}
