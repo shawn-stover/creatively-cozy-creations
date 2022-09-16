298c4161bc6cd0c49a56a867c9b486af0a9354c5
shawn-stover <shawn.stover.it.cs@gmail.com>
2022-09-16 05:14:19 -0700

    Implement Admin Dashboard to Track Sales
    Implement Tracker for Admins to track sales

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
 // Imports
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import Product from '../models/ProductModel.js';
+import Product from '../models/productModel.js';
 
 const productRouter = express.Router();
 
